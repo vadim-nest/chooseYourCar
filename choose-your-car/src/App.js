b015b585d2fc90aababc673fde1a34ca073441bd
Vadim Gnezdyshkin <vadim@gne.me.uk>
2023-06-16 10:36:05 +0100

    Implement router and add data

--- choose-your-car/src/App.js
+++ choose-your-car/src/App.js
@@ -1,19 +1,56 @@
 import './App.css';
 import SelectBox from './components/SelectBox.jsx';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Navigate
+} from 'react-router-dom';
 
-const options = [
-  { value: 'audi', label: 'AUDI' },
-  { value: 'bmw', label: 'BMW' }
-]
+const options = {
+  make: [
+    { value: 'audi', label: 'AUDI' },
+    { value: 'bmw', label: 'BMW' },
+    { value: 'vauxhal', label: 'VAUXHAL' },
+    { value: 'mercedes', label: 'MERCEDES' },
+    { value: 'peugeot', label: 'PEUGEOT' },
+    { value: 'renault', label: 'RENAULT' },
+  ],
+  colour: [
+    { value: 'blue', label: 'BLUE' },
+    { value: 'red', label: 'RED' },
+    { value: 'black', label: 'BLACK' },
+    { value: 'orange', label: 'ORANGE' },
+  ],
+};
 
 function App() {
-
   function handleSelectChange(value) {
-    console.log(`Selected: ${value}`)
+    console.log(`Selected: ${value}`);
   }
 
   return (
-    <SelectBox options={options} onSelectChange={handleSelectChange}/>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Navigate to="/make" />} />
+      {Object.keys(options).map((key, index) => {
+        return (
+          <Route
+            path={`/${key}`}
+            element={
+              <SelectBox
+                currentKey={key}
+                options={options[key]}
+                onSelectChange={handleSelectChange}
+                next={Object.keys(options)[index + 1]}
+              />
+            }
+          />
+        );
+      })}
+    </Routes>
+  </Router>
   );
 }
 
--- choose-your-car/src/components/SelectBox.jsx
+++ choose-your-car/src/components/SelectBox.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
-export default function SelectBox({ options, onSelectChange }) {
-  const [selected, setSelected] = useState(options[0]);
+export default function SelectBox({ currentKey, options, onSelectChange, next }) {
+  const [selected, setSelected] = useState(options[0].value);
+  const navigate = useNavigate();
 
   function handleChange(event) {
     setSelected(event.target.value);
     onSelectChange(event.target.value);
   }
+  console.log(options);
+
+  function handleNext() {
+    console.log('Next clicked');
+    navigate(`/${next}`); // This will always navigate to '/colour'. You may want to make this dynamic.
+  }
 
   return (
     <div className="select-box-window">
+      <h1>{currentKey.toUpperCase()}</h1>
       <select value={selected} onChange={handleChange}>
         {options.map((option, index) => (
           <option key={index} value={option.value}>
@@ -17,6 +26,9 @@ export default function SelectBox({ options, onSelectChange }) {
           </option>
         ))}
       </select>
+      <button className="button-next" type="button" onClick={handleNext}>
+          NEXT
+      </button>
     </div>
   );
 }
